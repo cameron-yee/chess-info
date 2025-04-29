43b02e1daa6f6d7478ffb1a8844424e4131631f1
Cameron Yee <yee.camero23@gmail.com>
2025-04-29 13:30:15 -0600

    fix: updates time flags

--- src/main.rs
+++ src/main.rs
@@ -21,13 +21,24 @@ struct Cli {
     #[arg(short, long, required=true)]
     username: String,
 
-    #[arg(short, long, required=true)]
-    year: String,
+    #[arg(long, default_value=None)]
+    start_month: Option<u32>,
+
+    #[arg(long, default_value=None)]
+    start_year: Option<i32>,
+
+    #[arg(long, default_value=None)]
+    end_month: Option<u32>,
+
+    #[arg(long, default_value=None)]
+    end_year: Option<i32>,
 }
 
 #[derive(Clone, Debug, Serialize)]
 struct Entry {
     count: u32,
+    eco_url: String,
+    eco: String,
     results: HashMap<String, u32>,
     average_accuracy: Option<f32>,
 }
@@ -66,37 +77,6 @@ struct Output {
 
 type GameResult = HashMap<String, u32>;
 type ParsedPgnTags<'a> = HashMap<&'a str, String>;
-
-async fn get_month_games(cli: &Cli, client: &reqwest::Client, month: u32) -> Option<ResponseJson> {
-    let result = client
-        .get(
-            format!(
-                "https://api.chess.com/pub/player/{}/games/{}/{:02}",
-                cli.username,
-                cli.year,
-                month
-            ))
-        .header(
-            "User-Agent",
-            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36"
-        )
-        .send()
-        .await;
-
-    let response = match result {
-        Ok(response) => response,
-        Err(e) => { println!("{}", e); panic!() },
-    };
-
-    let result = response.json::<ResponseJson>().await;
-    match result {
-        Ok(response) => Some(response),
-        Err(e) => {
-            println!("{}", e);
-            None
-        },
-    }
-}
 
 fn parse_pgn_tags(pgn: &str) -> ParsedPgnTags {
     let re = regex::RegexBuilder::new(r"^\[([^\]]+)]")
@@ -137,6 +117,8 @@ fn merge_entries(a: &Entry, b: &Entry) -> Entry {
             (None, Some(b_avg)) => Some(b_avg),
             (None, None) => None
         },
+        eco_url: a.eco_url.clone(),
+        eco: a.eco.clone(),
         results: merge_results(a.clone().results, b.clone().results),
     }
 }
@@ -159,13 +141,16 @@ fn get_user_pieces<'cli>(cli: &'cli Cli, tags: &ParsedPgnTags) -> Option<&'cli s
     let white = tags.get("White");
 
     match black {
-        Some(username) if *username == cli.username => return Some("Black"),
+        Some(username)
+            if *username.to_lowercase() == cli.username.to_lowercase()
+                => return Some("Black"),
         Some(_) => (),
         None => (),
     };
 
     match white {
-        Some(username) if *username == cli.username => Some("White"),
+        Some(username) if *username.to_lowercase() == cli.username.to_lowercase()
+            => Some("White"),
         Some(_) => None,
         None => None,
     }
@@ -221,6 +206,14 @@ fn add_game_to_output(cli: &Cli, json: &mut OutputJson, game: &Game) {
     let new_entry = Entry {
         average_accuracy: accuracy,
         count: 1,
+        eco: match tags.get("ECO") {
+            Some(t) => t.to_owned(),
+            None => String::from("")
+        },
+        eco_url: match tags.get("ECOUrl") {
+            Some(t) => t.to_owned(),
+            None => String::from("")
+        },
         results,
     };
 
@@ -232,13 +225,73 @@ fn add_game_to_output(cli: &Cli, json: &mut OutputJson, game: &Game) {
     json.insert(opening_name.to_string(), inserted_entry);
 }
 
-async fn run(cli: &Cli) {
+async fn get_month_games(cli: &Cli, month: u32, year: i32) -> Option<ResponseJson> {
     let now = Local::now();
-    let month = now.date_naive().month();
+    let current_month = now.date_naive().month();
+    let current_year = now.date_naive().year();
 
+    if current_year == year && month > current_month {
+        return None
+    }
     let client = reqwest::Client::new();
-    let response_futures = (1..month).map(|m| get_month_games(cli, &client, m));
-    let responses = join_all(response_futures).await;
+    let result = client
+        .get(
+            format!(
+                "https://api.chess.com/pub/player/{}/games/{}/{:02}",
+                cli.username,
+                year,
+                month
+            ))
+        .header(
+            "User-Agent",
+            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36"
+        )
+        .send()
+        .await;
+
+    let response = match result {
+        Ok(response) => response,
+        Err(e) => { println!("{}", e); panic!() },
+    };
+
+    let result = response.json::<ResponseJson>().await;
+    match result {
+        Ok(response) => Some(response),
+        Err(e) => {
+            println!("{}", e);
+            None
+        },
+    }
+}
+
+async fn get_games(cli: &Cli) -> Vec<Option<ResponseJson>> {
+    let now = Local::now();
+    let year = now.date_naive().year();
+
+    let start_month = cli.start_month.unwrap_or(1);
+    let end_month = match cli.end_month {
+        Some(m) => m + 1,
+        None => 13
+    };
+
+    let start_year = cli.start_year.unwrap_or(year);
+    let end_year = match cli.end_year {
+        Some(y) => y + 1,
+        None => year + 1,
+    };
+
+    let mut response_futures: Vec<_> = vec![];
+    for y in start_year..end_year {
+        for m in start_month..end_month {
+            response_futures.push(get_month_games(cli, m, y));
+        }
+    }
+
+    join_all(response_futures).await
+}
+
+async fn run(cli: &Cli) {
+    let responses = get_games(cli).await;
 
     let mut json: OutputJson = HashMap::new();
     for response in responses {
